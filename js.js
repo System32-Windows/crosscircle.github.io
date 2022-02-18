ed86bef7b4647779e7daeeb86940fee1e04035d0
MicrosoftWindowsX <danieldlugosz308@gmail.com>
2022-02-18 18:23:19 +0100

    obrazki

--- js.js
+++ js.js
@@ -1,4 +1,10 @@
 let cross, circle, gamer, polls, points1, points2, clicks = [];
 
+function start() {
+    cross = "<img src='g1.png'>"
+    circle = "<img src='g2.png'>"
+};
+
+
 function klik(id, number) {;
 };
