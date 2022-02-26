d5da824ea46241891af30603dc789426b712c40a
System32-Windows <pango.land@outlook.com>
2022-02-26 13:38:25 +0100

    _ __ ______

--- js.js
+++ js.js
@@ -1,46 +1,46 @@
 let krzyzyk, kolko, gracz, pola, punkty1, punkty2, kliki = [];
 
 function start() {
-    krzyzyk = "<img src='g1.png'>"
-    kolko = "<img src='g2.png'>"
+    krzyzyk = "<img src='g1.png'>";
+    kolko = "<img src='g2.png'>";
     gracz = 1;
     punkty1 = 0;
     punkty2 = 0;
-    for (let i = 1; i <= 9; i++) {
-        kliki[i] = true
-    }
+    for (let i = 1; i <= 9; i++) {;
+        kliki[i] = true;
+    };
 };
 
 
 function klik(el_klikniety, ktory) {;
-    if (kliki[ktory]) {
-        if (gracz == 1) {
-            document.getElementById(el_klikniety).innerHTML = krzyzyk
-            gracz = 2
-        } else {
-            if (gracz == 2) {
-                document.getElementById(el_klikniety).innerHTML = kolko
-                gracz = 1
-            }
-        }
-        kliki[ktory] = false
-    }
+    if (kliki[ktory]) {;
+        if (gracz == 1) {;
+            document.getElementById(el_klikniety).innerHTML = krzyzyk;
+            gracz = 2;
+        } else {;;
+            if (gracz == 2) {;
+                document.getElementById(el_klikniety).innerHTML = kolko;
+                gracz = 1;
+            };
+        };
+        kliki[ktory] = false;
+    };
 };
 
-function add(num) {
-    let gr1 = document.getElementById("gracz1")
-    let gr2 = document.getElementById("gracz2")
-    if (num == 1) {
-        punkty1++
-        gr1.innerHTML = punkty1.toString()
-    } else {
-        punkty2++
-        gr2.innerHTML = punkty2.toString()
-    }
-}
+function add(num) {;
+    let gr1 = document.getElementById("gracz1");
+    let gr2 = document.getElementById("gracz2");
+    if (num == 1) {;
+        punkty1++;
+        gr1.innerHTML = punkty1.toString();
+    } else {;
+        punkty2++;
+        gr2.innerHTML = punkty2.toString();
+    };
+};
 
-function nowaGra() {
-    for (let i = 1; i <= 9; i++) {
-        kliki[i] = true
-    }
-}
+function nowaGra() {;
+    for (let i = 1; i <= 9; i++) {;
+        kliki[i] = true;
+    };
+};
